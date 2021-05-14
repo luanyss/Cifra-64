41e338c5f8f28ccccb52d26c34279f1d7b5b9b31
victorsilvajc021 <luanyss@outlook.com>
2021-05-13 22:19:33 -0300

    finalização do prejeto cifra 64

---
+++ cifra64.js
@@ -0,0 +1,22 @@
+var texto = '';
+
+var btn = document.getElementById('botao');
+
+var opc = document.getElementById('linguagem');
+
+var radio1 = document.getElementById('codificar');
+
+var radio2 = document.getElementById('decodificar');
+
+var incremento = document.getElementById('baseCesar')
+
+codificar.onclick = ()=>{
+    btn.innerText = 'Clique aqui para Codificar'
+}
+decodificar.onclick = ()=>{
+    btn.innerText = 'Clique aqui para Decodificar'
+}
+
+
+
+
